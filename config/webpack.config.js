b95fcc8d504c3a8a3c57637ed0acb0558d207363
DESKTOP-IGOR\Igor <igor.migasiewicz@gmail.com>
2018-09-26 16:48:58 +0200

    Syntax check

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -29,87 +29,87 @@ const dirApp = 'app/';
 const dotenvFile = devMode ? '.env.development' : '.env.production';
 const env = dotenv.config({'path': path.resolve(__dirname, dotenvFile)}).parsed;
 const envKeys = Object.keys(env).reduce((prev, next) => {
-	prev[`process.env.${next}`] = JSON.stringify(env[next]);
-	return prev;
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
 }, {});
 
 //
 // Start of config
 //
 module.exports = {
-	mode: devMode ? 'development' : 'production',
-	devtool: devMode ? 'eval-source-map' : false,
-	entry: {
-		'index': path.resolve(ROOT, dirApp, 'index.js')
-	},
-	output: {
-		path: path.resolve(ROOT, dirDistribution),
-		filename: 'scripts/[name].dist.js',
-		publicPath: '/'
-	},
-	resolve: {
-		// only to use jsx extensions for component files
-		extensions: ['*', '.js', '.jsx']
-	},
-	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader',
-				query: {
-					plugins: [
-						'@babel/plugin-proposal-class-properties',
-						'@babel/plugin-proposal-export-default-from'
-					]
-				}
-			},
-			{
-				test: /\.s?css$/,
-				exclude: path.resolve(ROOT, dirApp, 'styles'),
-				use: [
-					devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-					{
-						loader: 'css-loader',
-						options: {
-							sourceMap: true,
-							modules: true,
-    					localIdentName: '[name]_[local]_[hash:base64:5]',
-    					importLoaders: 1
-						}
-					},
-					{
-						loader: 'sass-loader',
-						options: {
-							sourceMap: true
-						}
-					}
-				]
-			},
-			{
-				test: /\.s?css$/,
-				include: path.resolve(ROOT, dirApp, 'styles'),
-				use: [
-					devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-					{
-						loader: 'css-loader',
-						options: {
-							sourceMap: true
-						}
-					},
-					{
-						loader: 'sass-loader',
-						options: {
-							sourceMap: true
-						}
-					}
-				]
-			},
-			{
-      	test: /\.(eot|svg|ttf|woff|woff2)$/,
-      	loader: 'file-loader',
+  mode: devMode ? 'development' : 'production',
+  devtool: devMode ? 'eval-source-map' : false,
+  entry: {
+    'index': path.resolve(ROOT, dirApp, 'index.js')
+  },
+  output: {
+    path: path.resolve(ROOT, dirDistribution),
+    filename: 'scripts/[name].dist.js',
+    publicPath: '/'
+  },
+  resolve: {
+    // only to use jsx extensions for component files
+    extensions: ['*', '.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          plugins: [
+            '@babel/plugin-proposal-class-properties',
+            '@babel/plugin-proposal-export-default-from'
+          ]
+        }
       },
-			{
+      {
+        test: /\.s?css$/,
+        exclude: path.resolve(ROOT, dirApp, 'styles'),
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              modules: true,
+              localIdentName: '[name]_[local]_[hash:base64:5]',
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.s?css$/,
+        include: path.resolve(ROOT, dirApp, 'styles'),
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader',
+      },
+      {
         test: /\.(png|jpe?g|gif)$/,
         use: [
           {
@@ -118,35 +118,35 @@ module.exports = {
           }
         ]
       }
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: path.resolve(ROOT, dirApp, 'template.html'),
-			filename: path.resolve(ROOT, dirDistribution, 'index.html'),
-			title: 'tescior',
-			inject: 'body'
-		}),
-		new MiniCssExtractPlugin({
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(ROOT, dirApp, 'template.html'),
+      filename: path.resolve(ROOT, dirDistribution, 'index.html'),
+      title: 'tescior',
+      inject: 'body'
+    }),
+    new MiniCssExtractPlugin({
       filename: 'styles/[name].dist.css',
       chunkFilename: 'styles/[id].dist.css',
     }),
     new webpack.DefinePlugin(envKeys)
-	],
-	optimization: {
-		minimizer: [
-			new OptimizeCSSAssetsPlugin({})
-		]
-	},
-	stats: {
-		colors: true
-	},
-	devServer: {
-		port: 4000,
-		open: true,
-		contentBase: path.resolve(ROOT, dirDistribution),
-		historyApiFallback: true,
-		publicPath: '/',
-		https: true
-	}
+  ],
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  devServer: {
+    port: 4000,
+    open: true,
+    contentBase: path.resolve(ROOT, dirDistribution),
+    historyApiFallback: true,
+    publicPath: '/',
+    https: true
+  }
 }
